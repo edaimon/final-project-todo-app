9438b1c62d8316d15728648ddaf8da4aefc264a4
edaimon <eledaimon@yahoo.com>
2022-12-12 13:05:39 +0100

    feat: supabase created

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,1 +1,45 @@
-export default defineStore()
+// /store/user.js
+
+import { defineStore } from "pinia";
+
+import { supabase } from "../supabase";
+
+export default defineStore("user", {
+  state() {
+    return {
+      user: null,
+    };
+  },
+
+  actions: {
+    async fetchUser() {
+      const user = await supabase.auth.user();
+
+      this.user = user;
+    },
+
+    async signUp(email, password) {
+      const { user, error } = await supabase.auth.signUp({
+        email: email,
+
+        password: password,
+      });
+
+      if (error) throw error;
+
+      if (user) this.user = user;
+    },
+
+    persist: {
+      enabled: true,
+
+      strategies: [
+        {
+          key: "user",
+
+          storage: localStorage,
+        },
+      ],
+    },
+  },
+});
