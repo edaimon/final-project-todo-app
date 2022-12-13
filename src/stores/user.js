2809e0da4618d1e0b93149e352577e2514e9d13a
edaimon <eledaimon@yahoo.com>
2022-12-13 13:26:21 +0100

    user imported in pinia :)

--- src/stores/user.js
+++ src/stores/user.js
@@ -17,20 +17,18 @@ export default defineStore("user", {
     },
     async signUp(email, password) {
       const response = await supabase.auth.signUp({
-        email: 'example@email.com',
-        password: 'example-password',
-      })
-      
-      const data = response.data
-      const error = response.error
-      
+        email: "example@email.com",
+        password: "example-password",
+      });
+
+      const data = response.data;
+      const error = response.error;
+
       if (error) throw error;
       if (user) this.user = data.user;
     },
 
-    async signIn(email, password){
-
-      
+    async signIn(email, password) {
       const { data, error } = await supabase.auth.signInWithPassword({
         email: email,
         password: password,
@@ -42,17 +40,17 @@ export default defineStore("user", {
       }
     },
 
-    async signOut(){
+    async signOut() {
       const { error } = await supabase.auth.signOut();
     },
-    persist: {
-      enabled: true,
-      strategies: [
-        {
-          key: "user",
-          storage: localStorage,
-        },
-      ],
-    },
+  },
+  persist: {
+    enabled: true,
+    strategies: [
+      {
+        key: "user",
+        storage: localStorage,
+      },
+    ],
   },
 });
