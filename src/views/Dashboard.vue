3772be385b7210ce370537b393dfe0620a631e46
jordi92al <jordi92al@gmail.com>
2022-12-14 13:48:46 +0100

    fix: add new task

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -4,15 +4,16 @@
     </div>
     <div class="flex justify-around"> 
         <div class="flex flex-col">
-            <h2>TO DO</h2>
+            <h2>TO DO (status 1)</h2>
             <div v-for="item in tasksStore.tasks" :key="item.id" class="">  
                     <div v-if="item.status === 1">
                         {{item.title}}
                         {{item.description}}
                         status:{{item.status}}
-                        <img src="../img/delete.png" @click="deleteTask(item.id)" alt="delete icon" width="15">    
-                        <img src="../img/edit.png" @click="switchValue()" alt="edit icon" width="15" >
-
+                        <div class="flex justify-end">
+                            <img src="../img/delete.png" @click="deleteTask(item.id)" alt="delete icon" width="15">    
+                            <img src="../img/edit.png" @click="switchValue()" alt="edit icon" width="15" >
+                        </div>  
                         <div v-if="value === true">    
                             <input type="text" v-model="item.title" name="item.id" id="item.id" placeholder="Introduce your new title" >
                             <input type="text" v-model="item.description" name="item.id" id="item.id" placeholder="Introduce your new description" >
@@ -20,19 +21,31 @@
                         </div> 
 
                     </div>
-            
+        
+            </div>
+            <div >
+            <!-- INSERTAR TAREAS -->
+            <img src="../img/icono_suma.png" @click="switchValueToDo()" alt="icono suma" width="15" class=" bg-slate-500 rounded-full">
+                <div v-if="valueToDo === true">
+                    <input type="text" v-model="title" name="task" id="task" placeholder="title">
+                    <input type="text" v-model="description" name="task" id="task" placeholder="description">
+                    <img src="../img/confirm_icon.png" @click="insert(), switchValue()"  alt="edit icon" width="15">
+                </div>
+                
             </div>
         </div>
         <div class="flex flex-col">
-            <h2>DOING</h2>
+            <h2>DOING (status 2)</h2>
             <div v-for="item in tasksStore.tasks" :key="item.id" class="flex flex-col"> 
                      <div v-if="item.status === 2">
                 
                         {{item.title}}
                         {{item.description}}
                         status:{{item.status}}
-                        <img src="../img/delete.png" @click="deleteTask(item.id)" alt="delete icon" width="15">
-                        <img src="../img/edit.png" @click="switchValue()" alt="edit icon" width="15" >
+                        <div class="flex justify-end">
+                            <img src="../img/delete.png" @click="deleteTask(item.id)" alt="delete icon" width="15">
+                            <img src="../img/edit.png" @click="switchValue()" alt="edit icon" width="15" >
+                        </div>
                             <div v-if="value === true">    
                                 <input type="text" v-model="item.title" name="item.id" id="item.id" placeholder="Introduce your new title" >
                                 <input type="text" v-model="item.description" name="item.id" id="item.id" placeholder="Introduce your new description" >
@@ -40,17 +53,29 @@
                             </div>    
                     </div>
              </div>
+             <div >
+            <!-- INSERTAR TAREAS -->
+                <img src="../img/icono_suma.png" @click="switchValueDoing()" alt="icono suma" width="15" class=" bg-slate-500 rounded-full">
+                <div v-if="valueDoing === true">
+                    <input type="text" v-model="title" name="task" id="task" placeholder="title">
+                    <input type="text" v-model="description" name="task" id="task" placeholder="description">
+                    <img src="../img/confirm_icon.png" @click="insert(), switchValue()"  alt="edit icon" width="15"  >
+                </div>
+                
+            </div>
          </div>
          <div class="flex flex-col">
-            <h2>DONE</h2>
+            <h2>DONE (status 3)</h2>
             <div v-for="item in tasksStore.tasks" :key="item.id" class="flex flex-col"> 
                      <div v-if="item.status === 3">
                 
                         {{item.title}}
                         {{item.description}}
                         status:{{item.status}}
-                        <img src="../img/delete.png" @click="deleteTask(item.id)" alt="delete icon" width="15">
-                        <img src="../img/edit.png" @click="switchValue()" alt="edit icon" width="15" >
+                        <div class="flex justify-end">
+                            <img src="../img/delete.png" @click="deleteTask(item.id)" alt="delete icon" width="15">
+                            <img src="../img/edit.png" @click="switchValue()" alt="edit icon" width="15" >
+                        </div>
                             <div v-if="value === true">    
                                 <input type="text" v-model="item.title" name="item.id" id="item.id" placeholder="Introduce your new title" >
                                 <input type="text" v-model="item.description" name="item.id" id="item.id" placeholder="Introduce your new description" >
@@ -58,16 +83,19 @@
                             </div>    
                     </div>
              </div>
+             <div >
+             <!-- INSERTAR TAREAS  -->
+            <img src="../img/icono_suma.png" @click="switchValueDone()" alt="icono suma" width="15" class=" bg-slate-500 rounded-full">
+                <div v-if="valueDone === true">
+                    <input type="text" v-model="title" name="task" id="task" placeholder="title">
+                    <input type="text" v-model="description" name="task" id="task" placeholder="description">
+                    <img src="../img/confirm_icon.png" @click="insert(), switchValue()"  alt="edit icon" width="15"  >
+                </div>
+                
+            </div>
          </div>
     </div>
     
-    <div>
-        <!-- INSERTAR TAREAS -->
-    <input type="text" v-model="title" name="task" id="task" placeholder="Introduce your new title">
-    <input type="text" v-model="description" name="task" id="task" placeholder="Introduce your new description">
-    <img src="../img/icono_suma.png" @click="insert()" alt="icono suma" width="15" class=" bg-slate-500 rounded-full">
-
-    </div>
 </template>
 
 <script>
@@ -82,6 +110,9 @@ export default {
             title: null,
             description: null,
             value: false,
+            valueToDo: false,
+            valueDoing: false,
+            valueDone: false,
         };
     },
     computed: {
@@ -113,7 +144,32 @@ export default {
                 this.value = true;
             }
             
-        }
+        },
+        switchValueToDo(){
+            if( this.valueToDo === true){
+                this.valueToDo = false;
+            }
+            else if( this.valueToDo === false){
+                this.valueToDo = true;
+            }
+            
+        },
+        switchValueDoing(){
+            if( this.valueDoing === true){
+                this.valueDoing = false;
+            }
+            else if( this.valueDoing === false){
+                this.valueDoing = true;
+            }    
+        },
+        switchValueDone(){
+            if( this.valueDone === true){
+                this.valueDone = false;
+            }
+            else if( this.valueDone === false){
+                this.valueDone = true;
+            }    
+        },
 
     },
     mounted() {
