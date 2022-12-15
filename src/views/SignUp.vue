eef6dc94cd6dcee3a472a524bd98c5e76b4cc119
jordi92al <jordi92al@gmail.com>
2022-12-15 15:35:02 +0100

    core: add img and signUp.vue CSS

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,14 +1,33 @@
 <template>
-    <div>
-        SignUp
+    <div class="flex justify-center items-center mt-52">
+        <div class="flex flex-col  bg-white p-10 w-1/3 h-96 rounded-l-xl">
+            <h2 class="m-5 text-center">SIGN IN</h2>
+            <input type="text" v-model="email" placeholder="Enter your mail" class=" border-2 border-blue-200 rounded-lg text-center pt-1 pb-1 placeholder-gray-900 m-5">
+            <input type="password" v-model="password" name="password" placeholder="Enter your password" class=" border-2 border-blue-200 rounded-lg text-center pt-1 pb-1 placeholder-gray-900 m-5">
+            <div class="flex justify-center pt-5">
+                <button @click="login()" class=" bg-blue-400 rounded-lg w-32 text-center">LOGIN!</button>
+            </div>            
+            <div class="flex justify-center pt-5">
+             <p>you dont have an account?</p>
+             <button @click="switchImage = !switchImage" class="pl-5"> Register!</button>
+            </div>
 
-        <div>
-            <input type="text" v-model="email" placeholder="Enter your mail">
-            <input type="password" v-model="password" name="password" placeholder="Enter your password">
          </div>
-        <button @click="register()">Register!</button>
-        <button @click="login()">Login!</button>
-        <button @click="signOut"> Signout! </button>
+
+         <div class="flex flex-col  bg-white p-10 w-1/3 h-96 rounded-r-xl">
+            <h2 class="m-5 text-center"> SIGN UP</h2>
+            <input type="text" v-model="email" placeholder="Enter your mail" class=" border-2 border-blue-200 rounded-lg text-center pt-1 pb-1 placeholder-gray-900 m-5">
+            <input type="password" v-model="password" name="password" placeholder="Enter your password" class=" border-2 border-blue-200 rounded-lg text-center pt-1 pb-1 placeholder-gray-900 m-5">
+            <div class="flex justify-center pt-5">
+                <button @click="register()" class=" bg-blue-400 rounded-lg w-32">REGISTER!</button>
+            </div>
+            <div class="flex justify-center pt-5">
+                <p>Already a user?</p>
+                <button @click="switchImage = !switchImage" class="pl-5">Login!</button>
+            </div>
+        </div>
+<!--         <button @click="signOut"> Signout! </button> -->
+        <img src="../img/presentation_1.jpg" alt="img_presentation" width="700" class="imgPosition rounded-xl" :class="{'imgPositionSwitch': switchImage}">
     </div>
 </template>
 
@@ -27,6 +46,7 @@ export default {
         return{
             email: null,
             password: null,
+            switchImage: false,
         }
     },
 
@@ -40,6 +60,13 @@ export default {
                 this.$router.push("Dashboard");
             } 
         },
+
+        switchFormRegister(){
+
+
+
+        },
+
         signOut(){
             this.userStore.signOut()
         },
@@ -47,3 +74,20 @@ export default {
     },
 };
 </script>
+
+<style>
+body{
+    background-color: #8ECAE6;
+}
+
+.imgPosition{
+    position: absolute;
+    right: 280px;
+    transition: transform 1s;
+}
+
+.imgPositionSwitch{
+
+    transform: translateX(-680px);
+}
+</style>
