19a599f32d0ac6a5839dd1b9cfce1717a96465e7
jordi92al <jordi92al@gmail.com>
2022-12-17 04:02:51 +0100

    fix: input size

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -6,9 +6,9 @@
             <div v-if="switchForm === false" class="sizeContainer flex flex-col justify-around bg-white  min-w-fit"> <!-- PONER AQUI EL FALSE SE MOSTRARA EL FORM DEL SIGNIN -->
                 <div class="sizeContainerForm flex flex-col justify-center">
                     <h2 class="m-5 text-center text-5xl font-bold textColor">SIGN IN</h2>
-                    <form action="submit" @submit.prevent="login()" class="flex flex-col justify-evenly h-1/3">
-                        <input type="text" v-model="email" placeholder="Enter your email" class=" border-2 border-blue-200 rounded-lg text-center pt-1 pb-1 placeholder-gray-900 ml-28 mr-28 h-14">
-                        <input type="password" v-model="password" name="password" placeholder="Enter your password" class=" border-2 border-blue-200 rounded-lg text-center pt-1 pb-1 placeholder-gray-900 ml-28 mr-28 h-14">
+                    <form action="submit" @submit.prevent="login()" class="flex flex-col justify-evenly items-center h-1/3">
+                        <input type="text" v-model="email" placeholder="Enter your email" class="w-1/2 border-2 border-blue-200 rounded-lg text-center pt-1 pb-1 placeholder-gray-900 h-14">
+                        <input type="password" v-model="password" name="password" placeholder="Enter your password" class="w-1/2 border-2 border-blue-200 rounded-lg text-center pt-1 pb-1 placeholder-gray-900 h-14">
                         <div class="flex justify-center pt-5">
                             <button class="loginBtt rounded-lg w-64 h-16 text-center text-white font-bold shadow-xl">LOGIN!</button>
                         </div>     
@@ -25,9 +25,9 @@
             <div v-if="switchForm === true" class="sizeContainer flex flex-col items-end bg-white min-w-fit">  <!--PONER QUE SI LA VARIABLE SE CONVIERTE EN TRU SE MUESTRA ESTA -->
                 <div class="sizeContainerForm flex flex-col justify-center">
                     <h2 class="m-5 text-center text-5xl font-bold textColor"> CREATE AN ACCOUNT</h2>
-                    <form action="sumbit" @submit.prevent="register()"  class="flex flex-col justify-evenly h-1/3">
-                            <input type="text" v-model="email" placeholder="Enter your mail" class=" border-2 border-blue-200 rounded-lg text-center pt-1 pb-1 placeholder-gray-900 ml-28 mr-28 h-14">
-                            <input type="password" v-model="password" name="password" placeholder="Enter your password" class=" border-2 border-blue-200 rounded-lg text-center pt-1 pb-1 placeholder-gray-900 ml-28 mr-28 h-14">
+                    <form action="sumbit" @submit.prevent="register()"  class="flex flex-col justify-evenly items-center h-1/3">
+                            <input type="text" v-model="email" placeholder="Enter your mail" class=" w-1/2 border-2 border-blue-200 rounded-lg text-center pt-1 pb-1 placeholder-gray-900 h-14">
+                            <input type="password" v-model="password" name="password" placeholder="Enter your password" class="w-1/2 border-2 border-blue-200 rounded-lg text-center pt-1 pb-1 placeholder-gray-900 h-14">
                         
                         <div class="flex justify-center pt-5">
                             <button class="loginBtt rounded-lg w-64 h-16 text-center text-white font-bold shadow-xl">REGISTER!</button>
